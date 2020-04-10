af587b1c21adb60ccba8ab5f8b55e966082a7491
Mikhail Pershukov <dev@76543210.ru>
2020-04-10 23:45:32 +0300

    2. Не перекрывать equals hashCode[#269054]

---
+++ chapter_001/src/test/java/ru/job4j/job4jdesign/map/MapTest.java
@@ -0,0 +1,20 @@
+package ru.job4j.job4jdesign.map;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapTest {
+
+    @Test
+    public void printMap() {
+        Map<User, Object> map = new HashMap<>();
+        User user1 = new User("Ivan", 1, null);
+        User user2 = new User("Ivan", 1, null);
+        map.put(user1, "user1");
+        map.put(user2, "user2");
+        System.out.println(map);
+    }
+
+}
