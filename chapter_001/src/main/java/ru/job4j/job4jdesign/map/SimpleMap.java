05a10d574598d41ef31c6061ef171deec196a3cf
Mikhail Pershukov <dev@76543210.ru>
2020-04-13 01:38:07 +0300

    8. Реализовать собственную структуру данных - HashMap[#269057]

---
+++ chapter_001/src/main/java/ru/job4j/job4jdesign/map/SimpleMap.java
@@ -0,0 +1,184 @@
+package ru.job4j.job4jdesign.map;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class SimpleMap<K, V> implements Iterable<SimpleMap.Entry<K, V>> {
+    private Object[] table;
+    private final int fTABLESIZE;
+    private int size = 0;
+    private int modCount = 0;
+
+    public SimpleMap() {
+        fTABLESIZE = 16;
+        table = new Object[fTABLESIZE];
+    }
+
+    public V put(K key, V value) {
+        int hash = Math.abs(key.hashCode() % fTABLESIZE);
+        Node node = new Node(hash, key, value);
+        Node nodeInBucket = (Node) table[hash];
+        Node previousNodeInBucket = null;
+        if (nodeInBucket == null) {
+            table[hash] = node;
+            size++;
+            modCount++;
+        } else {
+            do {
+                if (nodeInBucket.key.equals(node.key) && nodeInBucket.key.hashCode() == node.key.hashCode()) {
+                    V result = nodeInBucket.value;
+                    nodeInBucket.value = node.value;
+                    modCount++;
+                    return result;
+                }
+                previousNodeInBucket = nodeInBucket;
+                nodeInBucket = nodeInBucket.next;
+            } while (nodeInBucket != null);
+            previousNodeInBucket.next = node;
+            modCount++;
+            size++;
+        }
+        return null;
+    }
+
+    public V get(K key) {
+        int hash = Math.abs(key.hashCode() % fTABLESIZE);
+        Node nodeInBucket = (Node) table[hash];
+        if (nodeInBucket != null) {
+            do {
+                if (nodeInBucket.key.equals(key) && nodeInBucket.key.hashCode() == key.hashCode()) {
+                    return nodeInBucket.value;
+                }
+                nodeInBucket = nodeInBucket.next;
+            } while (nodeInBucket != null);
+        }
+        return null;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public Iterator<Entry<K, V>> iterator() {
+        return new SimpleMapIterator();
+    }
+
+    private class Node {
+        public int hash;
+        public K key;
+        public V value;
+        public Node next;
+
+        public Node(int hash, K key, V value) {
+            this.hash = hash;
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    public static class Entry<K, V> {
+        private K key;
+        private V value;
+
+        public Entry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+
+            Entry<?, ?> entry = (Entry<?, ?>) o;
+
+            if (key != null ? !key.equals(entry.key) : entry.key != null) {
+                return false;
+            }
+            return value != null ? value.equals(entry.value) : entry.value == null;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = key != null ? key.hashCode() : 0;
+            result = 31 * result + (value != null ? value.hashCode() : 0);
+            return result;
+        }
+
+        @Override
+        public String toString() {
+            return key + " = " + value;
+        }
+    }
+
+
+    private class SimpleMapIterator implements Iterator<Entry<K, V>> {
+        private int expectedModCount;
+        private Entry<K, V> founded;
+        private int tableCellNumber = 0;
+        private Node lastNode;
+
+        public SimpleMapIterator() {
+            expectedModCount = modCount;
+        }
+
+        @Override
+        public boolean hasNext() {
+            if (expectedModCount != modCount) {
+                throw new ConcurrentModificationException();
+            }
+            if (founded != null) {
+                return true;
+            }
+
+            if (lastNode != null) {
+                if (lastNode.next != null) {
+                    founded = new Entry<>(lastNode.next.key, lastNode.next.value);
+                    lastNode = lastNode.next;
+                    return true;
+                } else {
+                    lastNode = null;
+                    tableCellNumber++;
+                }
+            }
+
+            while (tableCellNumber < fTABLESIZE) {
+                Node node = (Node) table[tableCellNumber];
+                if (node != null) {
+                    lastNode = node;
+                    break;
+                }
+                tableCellNumber++;
+            }
+            if (tableCellNumber == fTABLESIZE) {
+                return false;
+            }
+            founded = new Entry<>(lastNode.key, lastNode.value);
+            return true;
+        }
+
+        @Override
+        public Entry<K, V> next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            Entry<K, V> result = founded;
+            founded = null;
+            return result;
+        }
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/job4jdesign/map/SimpleMapTest.java
@@ -0,0 +1,192 @@
+package ru.job4j.job4jdesign.map;
+
+import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Set;
+
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.hasItemInArray;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertNull;
+
+public class SimpleMapTest {
+
+    @Test
+    public void addThreeObjectsAndSizeIsThree() {
+        SimpleMap<Object, Object> map = new SimpleMap<>();
+        map.put(new Object(), new Object());
+        map.put(new Object(), new Object());
+        map.put(new Object(), new Object());
+        assertThat(map.getSize(), is(3));
+    }
+
+    @Test
+    public void addThreeObjectsWithSameHashAndSizeIsThree() {
+        SimpleMap<Object, Object> map = new SimpleMap<>();
+        map.put(1, "test1");
+        map.put(1 + 16, "test2");
+        map.put(1 + 32, "test3");
+        assertThat(map.getSize(), is(3));
+    }
+
+    @Test
+    public void addThreeObjectsWithSameHashAndGetFirst() {
+        SimpleMap<Object, Object> map = new SimpleMap<>();
+        map.put(1, "test1");
+        map.put(1 + 16, "test2");
+        map.put(1 + 32, "test3");
+        assertThat(map.get(1), is("test1"));
+    }
+
+    @Test
+    public void addThreeObjectsWithSameHashAndGetSecond() {
+        SimpleMap<Object, Object> map = new SimpleMap<>();
+        map.put(1, "test1");
+        map.put(1 + 16, "test2");
+        map.put(1 + 32, "test3");
+        assertThat(map.get(17), is("test2"));
+    }
+
+    @Test
+    public void addThreeObjectsWithSameHashAndGetLast() {
+        SimpleMap<Object, Object> map = new SimpleMap<>();
+        map.put(1, "test1");
+        map.put(1 + 16, "test2");
+        map.put(1 + 32, "test3");
+        assertThat(map.get(33), is("test3"));
+    }
+
+    @Test
+    public void addThreeObjectsAndReplaceOneThanGetNewValue() {
+        SimpleMap<Object, Object> map = new SimpleMap<>();
+        map.put(1, "test1");
+        map.put(2, "test2");
+        map.put(3, "test3");
+        map.put(2, "newtest2");
+        assertThat(map.get(2), is("newtest2"));
+    }
+
+    @Test
+    public void addThreeObjectsAndReplaceOneWithReturnReplaced() {
+        SimpleMap<Object, Object> map = new SimpleMap<>();
+        map.put(1, "test1");
+        map.put(2, "test2");
+        map.put(3, "test3");
+        assertThat(map.put(2, "newtest2"), is("test2"));
+    }
+
+    @Test
+    public void addThreeObjectsAndReplaceOneThenSizeIsThree() {
+        SimpleMap<Object, Object> map = new SimpleMap<>();
+        map.put(1, "test1");
+        map.put(2, "test2");
+        map.put(3, "test3");
+        map.put(2, "newtest3");
+        assertThat(map.getSize(), is(3));
+    }
+
+    @Test
+    public void addThreeObjectsAndGetNonExistsKeyReturnsNull() {
+        SimpleMap<Object, Object> map = new SimpleMap<>();
+        map.put(1, "test1");
+        map.put(2, "test2");
+        map.put(3, "test3");
+        assertNull(map.get(-1));
+    }
+
+    @Test
+    public void addAndGetIterator() {
+        SimpleMap<Object, Object> map = new SimpleMap<>();
+        map.put(1, "test1");
+        map.put(2, "test2");
+        map.put(3, "test3");
+        assertThat(map, containsInAnyOrder(new SimpleMap.Entry<Object, Object>(1, "test1"),
+                new SimpleMap.Entry<Object, Object>(2, "test2"),
+                new SimpleMap.Entry<Object, Object>(3, "test3")));
+    }
+
+    @Test
+    public void iteratorAllowManyHasNext() {
+        SimpleMap<Object, Object> map = new SimpleMap<>();
+        map.put(1, "test1");
+        map.put(17, "test2");
+        map.put(33, "test3");
+        map.put(5, "test4");
+        map.put(21, "test5");
+        Iterator<SimpleMap.Entry<Object, Object>> iterator = map.iterator();
+        iterator.hasNext();
+        iterator.hasNext();
+        iterator.hasNext();
+        iterator.hasNext();
+        iterator.hasNext();
+        iterator.hasNext();
+        iterator.hasNext();
+        Set<SimpleMap.Entry<Object, Object>> set = Set.of(
+                new SimpleMap.Entry<>(1, "test1"),
+                new SimpleMap.Entry<>(17, "test2"),
+                new SimpleMap.Entry<>(33, "test3"),
+                new SimpleMap.Entry<>(5, "test4"),
+                new SimpleMap.Entry<>(21, "test5"));
+        assertThat(set, hasItem(iterator.next()));
+        assertThat(set, hasItem(iterator.next()));
+        assertThat(set, hasItem(iterator.next()));
+        assertThat(set, hasItem(iterator.next()));
+        assertThat(set, hasItem(iterator.next()));
+    }
+
+
+    @Test
+    public void addThreeWithSameHashAndTwoWithOtherAndGetIterator() {
+        SimpleMap<Object, Object> map = new SimpleMap<>();
+        map.put(1, "test1");
+        map.put(17, "test2");
+        map.put(33, "test3");
+        map.put(5, "test4");
+        map.put(21, "test5");
+        Iterator<SimpleMap.Entry<Object, Object>> iterator = map.iterator();
+        Set<SimpleMap.Entry<Object, Object>> set = new java.util.HashSet<>(Set.of(
+                new SimpleMap.Entry<>(1, "test1"),
+                new SimpleMap.Entry<>(17, "test2"),
+                new SimpleMap.Entry<>(33, "test3"),
+                new SimpleMap.Entry<>(5, "test4"),
+                new SimpleMap.Entry<>(21, "test5")));
+        SimpleMap.Entry<Object, Object> entry = iterator.next();
+        assertThat(set, hasItem(entry));
+        set.remove(entry);
+        entry = iterator.next();
+        assertThat(set, hasItem(entry));
+        set.remove(entry);
+        entry = iterator.next();
+        assertThat(set, hasItem(entry));
+        set.remove(entry);
+        entry = iterator.next();
+        assertThat(set, hasItem(entry));
+        set.remove(entry);
+        entry = iterator.next();
+        assertThat(set, hasItem(entry));
+        set.remove(entry);
+        assertThat(set.size(), is(0));
+    }
+
+    @Test (expected = NoSuchElementException.class)
+    public void addThreeWithSameHashAndTwoWithOtherAndGetNoSuchElementException() {
+        SimpleMap<Object, Object> map = new SimpleMap<>();
+        map.put(1, "test1");
+        map.put(1 + 16, "test2");
+        map.put(1 + 32, "test3");
+        map.put(5, "test4");
+        map.put(5 + 16, "test5");
+        Iterator<SimpleMap.Entry<Object, Object>> iterator = map.iterator();
+        iterator.next();
+        iterator.next();
+        iterator.next();
+        iterator.next();
+        iterator.next();
+        iterator.next();
+    }
+
+}
