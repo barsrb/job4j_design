06f83428efa21c9f7d01ef836012d5ff89325bfb
Mikhail Pershukov <dev@76543210.ru>
2020-05-13 23:38:11 +0300

    2. Что такое TDD?[#269094]

---
+++ chapter_005/src/test/java/ru/job4j/tdd/CinemaTest.java
@@ -0,0 +1,51 @@
+package ru.job4j.tdd;
+
+import org.junit.Test;
+
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertNull;
+
+public class CinemaTest {
+
+    @Test
+    public void buy() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, Calendar.NOVEMBER, 10, 23, 0);
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+        assertThat(ticket, is(new Ticket3D()));
+    }
+
+    @Test
+    public void whenPlaceNotEmpty() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, Calendar.NOVEMBER, 10, 23, 0);
+        cinema.buy(account, 1, 1, date);
+        Ticket wrongTicket = cinema.buy(account, 1, 1, date);
+        assertNull(wrongTicket);
+    }
+
+    @Test
+    public void find() {
+        Cinema cinema = new Cinema3D();
+        cinema.add(new Session3D());
+        List<Session> sessions = cinema.find(session -> true);
+        assertThat(sessions, is(Collections.singletonList(new Session3D())));
+    }
+
+    @Test
+    public void notFound() {
+        Cinema cinema = new Cinema3D();
+        cinema.add(new Session3D());
+        List<Session> sessions = cinema.find(session -> false);
+        assertThat(sessions, is(Collections.emptyList()));
+    }
+}
